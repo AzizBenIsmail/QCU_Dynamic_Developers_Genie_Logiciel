de9e426de8148a492e847d76dbad320b1fbebf5d
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-02-01 17:31:53 +0100

    Quiz app

---
+++ Dynamic developers/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Tout d'abord, qu'est-ce que Scrum ?",
+    answer: "Un cadre de travail (framework) pour le développement, la livraison et la maintenance de produits complexes.",
+    options: [
+      "Un cadre de travail (framework) pour rapprocher les développeurs (dev) et les opérationnels (ops)",
+      "Un cadre de travail (framework) pour le développement, la livraison et la maintenance de produits complexes.",
+      "Un cadre de travail (framework) pour l'élaboration, la conception et la construction de logiciels conviviaux (user-friendly)",
+      "Un cadre de travail (framework) pour optimiser les indicateur de performance clé (KPI) des équipes agile"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quelles sont les 5 valeurs de Scrum ?",
+    answer: "Engagement, Courage, Focus, Ouverture et Respect",
+    options: [
+      "Agilité, Sociabilité, Qualité, Solidarité, Adaptation",
+      "Apprendre, Partager, Exprimer, Adapter, S'engager",
+      "Responsabilisation (Empowerment), Transparence, Ouverture d'esprit, Tolérance, Gentillesse (Kindness)",
+      "Engagement, Courage, Focus, Ouverture et Respect"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Que contient le Backlog Produit tel que défini par Scrum ?",
+    answer: "Les éléments du Backlog Produit",
+    options: [
+      "Les éléments du Backlog Produit",
+      "Les User Stories",
+      " Les User Stories et les bugs",
+      "Les User Stories et les tâches techniques"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Dans quel(s) événement(s) Scrum la présence du Product Owner est-elle obligatoire ?",
+    answer: "Toutes sauf les Mêlées Quotidiennes (Daily Scrum)",
+    options: [
+      "Tous",
+      "Aucun ",
+      "Toutes sauf les Mêlées Quotidiennes (Daily Scrum) et les rétrospectives de Sprint",
+      "Toutes sauf les Mêlées Quotidiennes (Daily Scrum)"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Au fil du Sprint, quel élément peut-on faire évoluer ?",
+    answer: "Le périmètre",
+    options: [
+      "Le périmètre",
+      "Aucun de ces éléments",
+      "Les objectifs de qualité",
+      "L'objectif du Sprint"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
